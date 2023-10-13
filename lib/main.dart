0f6ce07a3f9d4590519111b8dda51575f3bbfe6e
qwertycvb(HeeRyun, Jin <jins4218@gmail.com>
2023-10-14 03:05:22 +0900

    feat: apply Pretendard font
    Signed-off-by: qwertycvb(HeeRyun, Jin <jins4218@gmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return GetMaterialApp(
       theme: ThemeData(
+        fontFamily: 'Pretendard',
         useMaterial3: true,
       ),
       initialBinding: AppBinding(),
