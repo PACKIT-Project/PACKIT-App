bd7941b56e82a32d0cbbd4bef0988151e55e3248
qwertycvb(HeeRyun, Jin <jins4218@gmail.com>
2023-10-22 21:40:04 +0900

    refactor: apply login page design change
    Signed-off-by: qwertycvb(HeeRyun, Jin <jins4218@gmail.com>

--- lib/presentation/authentication/views/login_page.dart
+++ lib/presentation/authentication/views/login_page.dart
@@ -15,13 +15,13 @@ class LoginPage extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            SvgPicture.asset("assets/images/packit.svg", width: 169.98.w, height: 50.52.w),
-            SizedBox(height: 5.71.w),
+            SvgPicture.asset("assets/images/packit.svg", width: 211.12405.w, height: 54.27002.h),
+            SizedBox(height: 17.16.h),
             Text(
-              "빈틈없는 여행 준비, 패킷",
-              style: TextStyle(fontSize: 18.53.sp, fontWeight: FontWeight.w600),
+              "함께하는 여행 준비의 시작",
+              style: TextStyle(fontSize: 20.sp, fontWeight: FontWeight.w500),
             ),
-            SizedBox(height: 115.51.w),
+            SizedBox(height: 115.51.h),
             SSOLoginButton(
               "카카오로 시작하기",
               iconName: "kakao",
@@ -30,10 +30,10 @@ class LoginPage extends StatelessWidget {
                 Navigator.pushNamed(context, RoutePath.onboarding);
               },
             ),
-            SizedBox(height: 10.w),
+            SizedBox(height: 10.h),
             SSOLoginButton(
               "다른 방법으로 시작하기",
-              border: Border.all(color: const Color(0xFF6C747C), width: 0.8.w),
+              border: Border.all(color: const Color(0xFFBBC1C6), width: 0.8.w),
               color: Colors.white,
               onPressed: () async => showAnotherLoginMethodModal(context),
             ),
@@ -47,66 +47,53 @@ class LoginPage extends StatelessWidget {
     return showModalBottomSheet(
       context: context,
       backgroundColor: Colors.transparent,
-      barrierColor: Colors.black.withOpacity(0.6),
+      barrierColor: Colors.black.withOpacity(0.3),
       elevation: 0,
       builder: (context) {
-        return Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Container(
-              width: 40.w,
-              height: 6.w,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(37),
-                color: Colors.white.withOpacity(0.39),
+        return Padding(
+          padding: EdgeInsets.fromLTRB(16.w, 0, 16.w, 28.h),
+          child: Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(12),
+              color: Colors.white,
+            ),
+            child: SizedBox(
+              width: double.maxFinite,
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  SizedBox(height: 10.28.w),
+                  Container(
+                    width: 40.w,
+                    height: 4.h,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(37),
+                      color: const Color(0x63D6D6D6),
+                    ),
+                  ),
+                  SizedBox(height: 19.36.h),
+                  Text("다른 방법으로 시작하기", style: TextStyle(fontSize: 18.sp, fontWeight: FontWeight.w600)),
+                  SizedBox(height: 21.36.h),
+                  SSOLoginButton(
+                    "네이버로 시작하기",
+                    iconName: "naver",
+                    color: const Color(0xFF03C75A),
+                    textColor: Colors.white,
+                    onPressed: () {},
+                  ),
+                  SizedBox(height: 10.h),
+                  SSOLoginButton(
+                    "Apple로 시작하기",
+                    iconName: "apple",
+                    color: Colors.black,
+                    textColor: Colors.white,
+                    onPressed: () {},
+                  ),
+                  SizedBox(height: 33.h),
+                ],
               ),
             ),
-            SizedBox(height: 9.w),
-            Container(
-              decoration: const BoxDecoration(
-                borderRadius: BorderRadius.only(topLeft: Radius.circular(16), topRight: Radius.circular(16)),
-                color: Colors.white,
-              ),
-              child: SizedBox(
-                width: double.maxFinite,
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    SizedBox(height: 23.55.w),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      children: [
-                        Text("다른 방법으로 시작하기", style: TextStyle(fontSize: 18.sp, fontWeight: FontWeight.w700)),
-                        SizedBox(width: 71.37.w),
-                        GestureDetector(
-                          onTap: () => Navigator.pop(context),
-                          child: Icon(Icons.close, size: 24.w),
-                        ),
-                        SizedBox(width: 25.13.w),
-                      ],
-                    ),
-                    SizedBox(height: 21.45.w),
-                    SSOLoginButton(
-                      "네이버로 시작하기",
-                      iconName: "naver",
-                      color: const Color(0xFF03C75A),
-                      textColor: Colors.white,
-                      onPressed: () {},
-                    ),
-                    SizedBox(height: 10.w),
-                    SSOLoginButton(
-                      "Apple로 시작하기",
-                      iconName: "apple",
-                      color: Colors.black,
-                      textColor: Colors.white,
-                      onPressed: () {},
-                    ),
-                    SizedBox(height: 56.w),
-                  ],
-                ),
-              ),
-            ),
-          ],
+          ),
         );
       },
     );
