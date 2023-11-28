d589eb012f9ef0b61e63c1e2c26cc0cccc8daeeb
qwertycvb(HeeRyun, Jin <jins4218@gmail.com>
2023-11-28 16:00:19 +0900

    feat: AppColor 추가
    Signed-off-by: qwertycvb(HeeRyun, Jin <jins4218@gmail.com>

---
+++ lib/app/config/app_color.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class AppColor {
+  // Main Blue
+  static const Color mainBlue = Color(0xFF0EA8FF);
+  static const Color mainBlueInActive = Color(0xFFBFEBFF);
+  static const Color mainBlue1 = Color(0xFFD1F1FF);
+
+  // Gray(Text, Icon)
+  static const Color coolGray100 = Color(0xFF5C6983);
+  static const Color coolGray200 = Color(0xFF455163);
+  static const Color coolGray300 = Color(0xFF2C3340);
+  static const Color coolGray400 = Color(0xFF1D222A);
+  static const Color coolGray500 = Color(0xFF0F1116);
+
+  // Gray(UI, Component)
+  static const Color gray1 = Color(0xFFF5F7FD);
+  static const Color gray2 = Color(0xFFF5F7FD);
+  static const Color gray3 = Color(0xFFF5F7FD);
+  static const Color gray4 = Color(0xFFF5F7FD);
+  static const Color gray5 = Color(0xFFF5F7FD);
+
+  // Alert
+  static const Color alert = Color(0xFFFF3049);
+}
