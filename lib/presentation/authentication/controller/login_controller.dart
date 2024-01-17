255441919f6e5abbf46f7797905102bb77f340dd
HeeRyun, Jin(qwertycvb) <jins4218@gmail.com>
2024-01-18 01:45:34 +0900

    feat: apple 로그인 기능 추가
    Signed-off-by: HeeRyun, Jin(qwertycvb) <jins4218@gmail.com>

--- lib/presentation/authentication/controller/login_controller.dart
+++ lib/presentation/authentication/controller/login_controller.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/foundation.dart';
 import 'package:get/get.dart';
 import 'package:kakao_flutter_sdk/kakao_flutter_sdk.dart';
+import 'package:sign_in_with_apple/sign_in_with_apple.dart';
 
 enum SSOType { kakao, naver, apple }
 
@@ -9,11 +10,15 @@ class LoginController extends GetxController {
 
   Future<void> login(SSOType ssoType) async {
     if (ssoType == SSOType.kakao) {
+      await _loginWithKakao();
     } else if (ssoType == SSOType.naver) {
-    } else if (ssoType == SSOType.apple) {}
+      await _loginWithNaver();
+    } else if (ssoType == SSOType.apple) {
+      await _loginWithApple();
+    }
   }
 
-  Future<void> loginWithKakao() async {
+  Future<void> _loginWithKakao() async {
     User user;
 
     try {
@@ -31,11 +36,19 @@ class LoginController extends GetxController {
     }
   }
 
-  Future<void> loginWithNaver() async {
+  Future<void> _loginWithNaver() async {
     // TODO : Implement login with Naver
   }
 
-  Future<void> loginWithApple() async {
-    // TODO : Implement login with Apple
+  Future<void> _loginWithApple() async {
+    AuthorizationCredentialAppleID credential;
+
+    try {
+      credential = await SignInWithApple.getAppleIDCredential(scopes: []);
+
+      if (kDebugMode) print(credential);
+    } catch (e) {
+      if (kDebugMode) print('애플 로그인 실패: $e');
+    }
   }
 }
