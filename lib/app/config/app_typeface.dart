0e5dbcd29e17170ff6ff287cd0f163d743c3a292
HeeRyun, Jin(qwertycvb) <jins4218@gmail.com>
2024-01-30 01:24:39 +0900

    refactor: AppTypeFace lineHeight 추가
    Signed-off-by: HeeRyun, Jin(qwertycvb) <jins4218@gmail.com>

--- lib/app/config/app_typeface.dart
+++ lib/app/config/app_typeface.dart
@@ -6,25 +6,25 @@ class AppTypeFace {
   static AppTypeFace to = AppTypeFace();
 
   // Title
-  TextStyle display1bold = TextStyle(fontSize: 24.sp, fontWeight: FontWeight.w700, color: AppColor.coolGray500);
-  TextStyle display2Semibold = TextStyle(fontSize: 20.sp, fontWeight: FontWeight.w600, color: AppColor.coolGray500);
-  TextStyle subHeading1Semibold = TextStyle(fontSize: 18.sp, fontWeight: FontWeight.w600, color: AppColor.coolGray500);
+  TextStyle display1bold = TextStyle(fontSize: 24.sp, fontWeight: FontWeight.w700, color: AppColor.coolGray500, letterSpacing: -0.5);
+  TextStyle display2Semibold = TextStyle(fontSize: 20.sp, fontWeight: FontWeight.w600, color: AppColor.coolGray500, letterSpacing: -0.5);
+  TextStyle subHeading1Semibold = TextStyle(fontSize: 18.sp, fontWeight: FontWeight.w600, color: AppColor.coolGray500, letterSpacing: -0.5);
   TextStyle subHeading2Semibold = TextStyle(fontSize: 18.sp, fontWeight: FontWeight.w700, color: AppColor.coolGray500);
   TextStyle subHeading3Semibold = TextStyle(fontSize: 22.sp, fontWeight: FontWeight.w600, color: AppColor.coolGray500);
-  TextStyle subHeading4Medium = TextStyle(fontSize: 20.sp, fontWeight: FontWeight.w500, color: AppColor.coolGray500);
+  TextStyle subHeading4Medium = TextStyle(fontSize: 20.sp, fontWeight: FontWeight.w500, color: AppColor.coolGray500, letterSpacing: -0.5);
   TextStyle hero1Bold = TextStyle(fontSize: 34.sp, fontWeight: FontWeight.w700, color: AppColor.coolGray500);
 
   // Text
-  TextStyle body1Medium = TextStyle(fontSize: 18.sp, fontWeight: FontWeight.w500, color: AppColor.coolGray500);
-  TextStyle body2Bold = TextStyle(fontSize: 16.sp, fontWeight: FontWeight.w700, color: AppColor.coolGray500);
-  TextStyle body3SemiBold = TextStyle(fontSize: 16.sp, fontWeight: FontWeight.w600, color: AppColor.coolGray500);
-  TextStyle body4SemiBold = TextStyle(fontSize: 14.sp, fontWeight: FontWeight.w600, color: AppColor.coolGray500);
+  TextStyle body1Medium = TextStyle(fontSize: 18.sp, fontWeight: FontWeight.w500, color: AppColor.coolGray500, letterSpacing: -0.5);
+  TextStyle body2Bold = TextStyle(fontSize: 16.sp, fontWeight: FontWeight.w700, color: AppColor.coolGray500, letterSpacing: -0.5);
+  TextStyle body3SemiBold = TextStyle(fontSize: 16.sp, fontWeight: FontWeight.w600, color: AppColor.coolGray500, letterSpacing: -0.5);
+  TextStyle body4SemiBold = TextStyle(fontSize: 14.sp, fontWeight: FontWeight.w600, color: AppColor.coolGray500, letterSpacing: -0.16);
   TextStyle body5SemiBold = TextStyle(fontSize: 17.sp, fontWeight: FontWeight.w600, color: AppColor.coolGray500);
   TextStyle body6Medium = TextStyle(fontSize: 15.sp, fontWeight: FontWeight.w500, color: AppColor.coolGray500);
 
   // Description
   TextStyle caption1Semibold = TextStyle(fontSize: 13.sp, fontWeight: FontWeight.w600, color: AppColor.coolGray500);
-  TextStyle caption2Medium = TextStyle(fontSize: 12.sp, fontWeight: FontWeight.w500, color: AppColor.coolGray500);
+  TextStyle caption2Medium = TextStyle(fontSize: 12.sp, fontWeight: FontWeight.w500, color: AppColor.coolGray500, letterSpacing: -0.12);
   TextStyle caption3Semibold = TextStyle(fontSize: 11.sp, fontWeight: FontWeight.w600, color: AppColor.coolGray500);
 
   // Button
