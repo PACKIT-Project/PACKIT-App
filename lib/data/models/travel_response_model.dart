b8b5e1a4d8e6a19d9157724940a419196edb70c2
HeeRyun, Jin(qwertycvb) <jins4218@gmail.com>
2024-02-08 00:51:24 +0900

    fix: TravelResponse 파싱 오류 수정
    Signed-off-by: HeeRyun, Jin(qwertycvb) <jins4218@gmail.com>

--- lib/data/models/travel_response_model.dart
+++ lib/data/models/travel_response_model.dart
@@ -1,4 +1,7 @@
+// ignore_for_file: invalid_annotation_target
+
 import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:intl/intl.dart';
 
 part 'travel_response_model.freezed.dart';
 part 'travel_response_model.g.dart';
@@ -9,11 +12,19 @@ class TravelResponseModel with _$TravelResponseModel {
     required int id,
     required String title,
     required String destination,
-    required DateTime startDate,
-    required DateTime endDate,
-    required int dDay,
+    @JsonKey(fromJson: dateFromJson) required DateTime startDate,
+    @JsonKey(fromJson: dateFromJson) required DateTime endDate,
+    @JsonKey(fromJson: dDayFromJson) required int dDay,
     required int memberNum,
   }) = _TravelResponseModel;
 
   factory TravelResponseModel.fromJson(Map<String, Object?> json) => _$TravelResponseModelFromJson(json);
+}
+
+int dDayFromJson(String value) {
+  return int.parse(value);
+}
+
+DateTime dateFromJson(String value) {
+  return DateFormat("yyyy.mm.dd").parse(value);
 }
--- lib/domain/entities/travel_response.dart
+++ lib/domain/entities/travel_response.dart
@@ -1,19 +1,30 @@
+// ignore_for_file: invalid_annotation_target
+
 import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:intl/intl.dart';
 
 part 'travel_response.freezed.dart';
 part 'travel_response.g.dart';
 
 @freezed
 class TravelResponse with _$TravelResponse {
-  const factory TravelResponse({
+  factory TravelResponse({
     required int id,
     required String title,
     required String destination,
-    required DateTime startDate,
-    required DateTime endDate,
-    required int dDay,
+    @JsonKey(fromJson: dateFromJson) required DateTime startDate,
+    @JsonKey(fromJson: dateFromJson) required DateTime endDate,
+    @JsonKey(fromJson: dDayFromJson) required int dDay,
     required int memberNum,
   }) = _TravelResponse;
 
   factory TravelResponse.fromJson(Map<String, Object?> json) => _$TravelResponseFromJson(json);
+}
+
+int dDayFromJson(String value) {
+  return int.parse(value);
+}
+
+DateTime dateFromJson(String value) {
+  return DateFormat("yyyy.mm.dd").parse(value);
 }
